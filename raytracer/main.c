57eb184b34dbee1ae41e06c85b00e539cc00dafc
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-09-18 09:00:44 +0200

    vector c class

---
+++ raytracer/main.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+// https://raytracing.github.io/books/RayTracingInOneWeekend.html
+ 
+#define TRUE 0
+#define FALSE 1
+
+struct image {
+    int width, height;
+    char* data; // rgba format (0..255)
+};
+
+void image_create(int width, int height, image* i) {
+    // check width and height not 0
+    image->width = width;
+    image->height = height;
+
+    image->data = malloc(width * height * 4);
+}
+
+int main () {
+    print("Test");
+    return 0;
+}
